35683d82fc7402a55550ab4f55a309f8e96d095c
never130 <never130@hotmail.com>
2023-03-17 01:31:02 -0300

    ej1

---
+++ src/com/ejercicio1/Main.java
@@ -0,0 +1,36 @@
+package com.ejercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. numericos
+        // 1.1 enteros
+        byte variable1 = 5;
+        System.out.println("tipo de dato byte: " + variable1);
+        short variable2 = 10;
+        System.out.println("tipo de dato short: " + variable2);
+        int variable3 = 10;
+        System.out.println("tipo de dato int: " + variable3);
+        long variable4 = 100;
+        System.out.println("tipo de dato long: " + variable4);
+
+        // 1.2 decimales
+        float variable5 = 5.5f;
+        System.out.println("tipo de dato float: " + variable5);
+        double variable6 = 5.5d;
+        System.out.println("tipo de dato double: " + variable6);
+
+
+        // 2. booleano
+        boolean variable7 = false;
+        System.out.println("tipo de dato boolean: " + variable7);
+        boolean variable8 = true;
+        System.out.println("tipo de dato boolean: " + variable8);
+
+        // 3. texto
+        char variable9 = 'a';
+        System.out.println("tipo de dato char: " + variable9);
+        String variable10 = "Lorem ispum dolor sit amot...";
+        System.out.println("tipo de dato String: " + variable10);
+
+    }
+}
